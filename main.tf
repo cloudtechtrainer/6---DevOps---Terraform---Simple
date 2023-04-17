ea3d531325654c9eabb4131ba489b795332be195
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-04-18 01:46:52 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+# main.tf
+
+# Create a new security group that allows incoming SSH traffic
+resource "aws_security_group" "ssh" {
+  name_prefix = "allow-ssh-and-http"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a new EC2 instance and associate it with the security group
+resource "aws_instance" "example" {
+  count = 4 
+  ami           = "ami-0f8aef1783704ebc9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+}
