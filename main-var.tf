36c02e05f7f23b888246b91154e5ae277f849404
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2024-08-06 22:38:34 +0530

    Update main-var.tf

--- main-var.tf
+++ main-var.tf
@@ -9,20 +9,3 @@ variable "key" {
   description = "The name of the EC2 key pair to associate with the instances."
   type        = string
 }
-
-# Create a new security group that allows incoming SSH traffic
-resource "aws_security_group" "ssh" {
-  name_prefix = "allow-ssh-and-http"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-}
