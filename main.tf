bbe4bdb2084eff5663a1cdd2a7901ae7c63a0b0b
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2024-08-06 22:44:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,6 @@ resource "aws_instance" "example" {
   count = 4 
   ami           = "ami-0d473344347276854"
   instance_type = "t2.micro"
-  key_name = "tf-key-pair"
+  key_name = "Test-1"
   vpc_security_group_ids = [aws_security_group.ssh.id]
 }
